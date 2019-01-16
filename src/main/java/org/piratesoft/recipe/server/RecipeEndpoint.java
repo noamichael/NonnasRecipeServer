6a202c202a278473f44afd68d4005e416aae4063
michaelkucinski <michaelkucinski@192.168.2.1>
2019-01-15 23:12:42 -0500

    Corrected CORS headers and INSERT sql

--- src/main/java/org/piratesoft/recipe/server/RecipeEndpoint.java
+++ src/main/java/org/piratesoft/recipe/server/RecipeEndpoint.java
@@ -10,6 +10,7 @@ import org.piratesoft.recipe.server.schema.RecipeResponse;
 import org.piratesoft.recipe.server.sql.MySql;
 import spark.Request;
 import spark.Response;
+import spark.ResponseTransformer;
 import spark.Route;
 import static spark.Spark.*;
 
@@ -20,27 +21,43 @@ import static spark.Spark.*;
 public class RecipeEndpoint {
 
     public static void setupEndpoints() {
-        
+
         final Gson gson = new GsonBuilder().disableHtmlEscaping().create();
-        
-        before((req, res) -> {
-            res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
-            res.header("Access-Control-Allow-Origin", "*");
-            res.header("Access-Control-Allow-Headers", "Content-Type,Authorization,X-Requested-With,Content-Length,Accept,Origin,");
-            res.header("Access-Control-Allow-Credentials", "true");
+
+        options("/*", (request, response) -> {
+
+            String accessControlRequestHeaders = request.headers("Access-Control-Request-Headers");
+            if (accessControlRequestHeaders != null) {
+                response.header("Access-Control-Allow-Headers", accessControlRequestHeaders);
+            }
+
+            String accessControlRequestMethod = request.headers("Access-Control-Request-Method");
+            if (accessControlRequestMethod != null) {
+                response.header("Access-Control-Allow-Methods", accessControlRequestMethod);
+            }
+
+            return "OK";
         });
-        
+
+        before((request, response) -> {
+            response.header("Access-Control-Allow-Origin", "*");
+            response.header("Access-Control-Request-Method", "GET,PUT,POST,DELETE,OPTIONS");
+            response.header("Access-Control-Allow-Headers", "*");
+            response.header("Access-Control-Allow-Credentials", "true");
+        });
+
         //GET methods
-        
+        final ResponseTransformer JSON = new JsonTransformer();
+
         get("/recipe-types", (req, res) -> {
             return new RecipeResponse<>(Arrays.asList(RecipeType.values()));
-        }, new JsonTransformer());
+        }, JSON);
 
         get("/recipes", sqlRoute((req, res, sql) -> {
             int page = Integer.valueOf(req.queryParamOrDefault("page", "1"));
             int count = Integer.valueOf(req.queryParamOrDefault("count", "1000"));
             return sql.getRecipes(page, count);
-        }), new JsonTransformer());
+        }), JSON);
 
         get("/recipes/:id", sqlRoute((req, res, sql) -> {
             int recipeId = Integer.valueOf(req.params(":id"));
@@ -50,24 +67,21 @@ public class RecipeEndpoint {
                 return "Recipe not found";
             }
             return new RecipeResponse<>(recipeOptional.get());
-        }), new JsonTransformer());
-        
+        }), JSON);
+
         //Save Methods
-        
         post("/recipes", sqlRoute((req, res, sql) -> {
             Recipe recipe = gson.fromJson(req.body(), Recipe.class);
             int id = sql.saveRecipe(recipe);
-            if(id == -1){
+            if (id == -1) {
                 res.status(500);
                 return "Could not save recipe. Unknown SQL error";
             }
             Recipe justId = new Recipe();
             justId.setId(id);
             return new RecipeResponse<>(justId);
-        }));
+        }), JSON);
     }
-    
-    
 
     static Route sqlRoute(SQLEndpoint endpoint) {
         return (req, res) -> {
@@ -82,6 +96,7 @@ public class RecipeEndpoint {
 
     @FunctionalInterface
     interface SQLEndpoint {
+
         Object handle(Request req, Response res, MySql sql) throws Exception;
     }
 
