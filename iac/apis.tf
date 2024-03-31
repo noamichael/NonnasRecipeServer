7abf8ebc73b6ad8d7fe000b5bfd4adbe77d53f65
Michael Kucinski <MichaelKucinski@gmail.com>
2024-03-31 18:12:08 -0400

    fix: terraform sucks

--- iac/apis.tf
+++ iac/apis.tf
@@ -2,14 +2,23 @@ variable "apis" {
   type = list(string)
   default = [ 
     "iam.googleapis.com",
-    "serviceusage.googleapis.com",
-    "cloudresourcemanager.googleapis.com",
     "storage.googleapis.com",
     "run.googleapis.com"
   ]
 }
 
+resource "google_project_service" "enable_service_usage" {
+  project = "${var.project_id}"
+  service = "serviceusage.googleapis.com"
+}
+
+resource "google_project_service" "enable_cloud_resource_manage" {
+  project = "${var.project_id}"
+  service = "cloudresourcemanager.googleapis.com"
+}
+
 resource "google_project_service" "enable_api" {
+  depends_on = [ google_project_service.enable_service_usage, google_project_service.enable_cloud_resource_manage]
   project = "${var.project_id}"
   for_each = toset(var.apis)
   service = each.value
