a6907c1cc8ad4a206d7ffc7b156153de46ab59e3
Michael Kucinski <michaelkucinski@gmail.com>
2024-12-08 17:50:03 -0500

    feat: backend secrets reader

--- iac/root/iam.tf
+++ iac/root/iam.tf
@@ -17,6 +17,12 @@ resource "google_project_iam_member" "backend_cloud_sql_client" {
   member  = "serviceAccount:${google_service_account.backend.email}"
 }
 
+resource "google_project_iam_member" "backend_secrets_accessor" {
+  project = var.project_id
+  role    = "roles/secretmanager.secretAccessor"
+  member  = "serviceAccount:${google_service_account.backend.email}"
+}
+
 resource "google_project_iam_member" "backend_artifact_registry_reader" {
   project = var.project_id
   role    = "roles/artifactregistry.reader"
@@ -32,6 +38,14 @@ resource "google_service_account_iam_binding" "cloud_run_ci_cd_workload_pool" {
   role               = "roles/iam.workloadIdentityUser"
   members = [
     "principalSet://iam.googleapis.com/projects/${data.google_project.project.number}/locations/global/workloadIdentityPools/github/attribute.repository/noamichael/NonnasRecipeServer"
+  ]
+}
+
+resource "google_service_account_iam_binding" "cloud_run_ci_cd_service_identity" {
+  service_account_id = google_service_account.backend.id
+  role               = "roles/iam.serviceAccountUser"
+  members = [
+    "serviceAccount:${google_service_account.cloud_run_ci_cd.email}"
   ]
 }
 
