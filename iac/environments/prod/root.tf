ce2169cdddbb6a05baccfc010116cd35f81188e8
Michael Kucinski <michaelkucinski@gmail.com>
2024-12-08 11:00:50 -0500

    feat: add secrets in IAC

--- iac/environments/prod/root.tf
+++ iac/environments/prod/root.tf
@@ -7,3 +7,13 @@ import {
   id = "projects/trans-radius-98822/instances/nonnas-recipes"
   to = module.root.google_sql_database_instance.nonnas_recipes
 }
+
+import {
+  id = "projects/trans-radius-98822/serviceAccounts/cloud-run-ci-cd@trans-radius-98822.iam.gserviceaccount.com"
+  to = module.root.google_service_account.cloud_run_ci_cd
+}
+
+import {
+  id = "projects/trans-radius-98822/serviceAccounts/nr-backend@trans-radius-98822.iam.gserviceaccount.com"
+  to = module.root.google_service_account.backend
+}
