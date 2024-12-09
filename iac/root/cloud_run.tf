573901f98aa249c8ad4a2fe2746c7babc4b207bd
Michael Kucinski <michaelkucinski@gmail.com>
2024-12-08 21:14:41 -0500

    feat(iac): add cloud run load balancing for frontend

--- iac/root/cloud_run.tf
+++ iac/root/cloud_run.tf
@@ -1,5 +1,11 @@
 data "google_cloud_run_v2_service" "backend" {
   project  = var.project_id
   name     = "backend"
-  location = "us-central1"
+  location = var.region
+}
+
+data "google_cloud_run_v2_service" "frontend" {
+  project  = var.project_id
+  name     = "frontend"
+  location = var.region
 }
--- iac/root/vars.tf
+++ iac/root/vars.tf
@@ -3,3 +3,12 @@ variable "project_id" {
   default     = "trans-radius-98822"
 }
 
+variable "domain" {
+  description = "the domain for the frontend"
+  default     = "recipes.michaelkucinski.com"
+}
+
+variable "region" {
+  description = "the GCP project region"
+  default     = "us-central1"
+}
