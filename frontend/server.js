56b77dac4fef6406dde77904cb41c918f015396b
Michael Kucinski <michaelkucinski@gmail.com>
2024-12-08 19:36:09 -0500

    feat(frontend): add token logic for cloud run

--- frontend/server.js
+++ frontend/server.js
@@ -5,6 +5,8 @@ const helmet = require("helmet");
 
 const app = express();
 const api = process.env.BACKEND_URL || 'backend:6789';
+const gcpMetadata = `http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/default/identity?audience=${api}`
+let gcpAuthToken = ''
 
 // Set security headers (excluding content policy)
 // since that breaks Angular's dyanimc styles
@@ -54,8 +56,45 @@ function doProxy(proxyHost, replace) {
     return proxy(proxyHost, {
         proxyReqPathResolver: (req) => {
             return req.url.replace(replace, '');
+        },
+        proxyReqOptDecorator: async (proxyReqOpts) => {
+            // is cloud run, get backend auth token
+            // TODO: cache this token for 30 minutes
+            if (process.env.K_SERVICE) {
+                const token = await getGCPAuthToken()
+                proxyReqOpts.headers['Authorization'] = `Bearer ${token}`
+
+            }
+
+            return proxyReqOpts;
         }
     });
+}
+
+const thirtyMinutes = 30 * 60 * 1000
+
+async function getGCPAuthToken() {
+    if (gcpAuthToken) {
+        return gcpAuthToken
+    }
+
+    console.log('Getting GCP Auth Token')
+
+    const tokenResponse = await fetch(gcpMetadata, {
+        method: 'GET',
+        headers: {
+            'Metadata-Flavor': 'Google'
+        }
+    })
+
+    // Clear the token after 30 minutes
+    setTimeout(() => gcpAuthToken = '', thirtyMinutes);
+
+    gcpAuthToken = tokenResponse.text()
+
+    console.log(`Cached token of size ${gcpAuthToken.length}`)
+
+    return gcpAuthToken
 }
 
 // Sets the Cache-Control header on the given request
