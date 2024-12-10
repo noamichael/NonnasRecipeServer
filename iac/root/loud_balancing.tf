213065b2d3c6ef3807c270d76d61a279951e27d8
Michael Kucinski <michaelkucinski@gmail.com>
2024-12-09 20:11:16 -0500

    feat(iac): switch to cloud run domain mapping

--- iac/root/loud_balancing.tf
+++ iac/root/loud_balancing.tf
@@ -1,41 +1,10 @@
-
-resource "google_compute_region_network_endpoint_group" "frontend_neg" {
-  name                  = "serverless-neg"
-  network_endpoint_type = "SERVERLESS"
-  region                = var.region
-  cloud_run {
-    service = data.google_cloud_run_v2_service.frontend.name
+resource "google_cloud_run_domain_mapping" "frontend" {
+  name     = var.domain
+  location = var.region
+  metadata {
+    namespace = var.project_id
   }
-}
-
-module "lb-http" {
-  source  = "terraform-google-modules/lb-http/google//modules/serverless_negs"
-  version = "~> 12.0"
-
-  name    = "frontend-lb"
-  project = var.project_id
-
-  ssl                             = true
-  managed_ssl_certificate_domains = [var.domain]
-  https_redirect                  = true
-
-  backends = {
-    default = {
-      description = "Frontend's backend"
-      groups = [
-        {
-          group = google_compute_region_network_endpoint_group.frontend_neg.id
-        }
-      ]
-
-      enable_cdn = false
-
-      iap_config = {
-        enable = false
-      }
-      log_config = {
-        enable = false
-      }
-    }
+  spec {
+    route_name = "frontend"
   }
 }
